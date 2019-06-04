d04ae81122d3a356d39a4d2164862a6d6665cb32
Alexander <janickiy@mail.ru>
2019-06-04 04:49:20 +0300

    bug fix

--- app/Controllers/Dashboard/TemplateController.php
+++ app/Controllers/Dashboard/TemplateController.php
@@ -5,6 +5,7 @@ namespace App\Controllers\Dashboard;
 use App\Models\Templates;
 use App\Controllers\Controller;
 use Respect\Validation\Validator as v;
+use App\Models\Category;
 
 class TemplateController extends Controller
 {
@@ -13,21 +14,63 @@ class TemplateController extends Controller
        return $this->view->render($response,'dashboard/template/index.twig');
    }
 
+    /**
+     * @param $request
+     * @param $response
+     * @return mixed
+     */
    public function create($request,$response)
    {
+       $category = Category::get();
 
-
-       return $this->view->render($response,'dashboard/template/create_edit.twig');
+       return $this->view->render($response,'dashboard/template/create_edit.twig', compact('category'));
    }
 
-   public function store()
+    /**
+     * @param $request
+     * @param $response
+     * @return mixed
+     */
+   public function store($request,$response)
    {
+       $validation = $this->validator->validate($request,[
+           'name' => v::stringType()->notEmpty()->length(1, 255),
+           'body' => v::stringType()->notEmpty(),
+           'prior' => v::numeric()->notEmpty(),
+           'categoryId' => v::numeric()->notEmpty(),
+       ]);
 
+       if ($validation->failed()) {
+           return $response->withRedirect($this->router->pathFor('admin.template.create'));
+       }
+
+       $template = Templates::create([
+           'name' => $request->getParam('name'),
+           'body' => $request->getParam('body'),
+           'prior' => $request->getParam('prior'),
+           'categoryId' => $request->getParam('categoryId'),
+       ]);
+
+       $this->flash->addMessage('info','Данные успешно добавлены');
+
+       return $response->withRedirect($this->router->pathFor('admin.main'));
    }
 
-   public function edit()
+    /**
+     * @param $id
+     * @param $request
+     * @param $response
+     * @return mixed
+     */
+   public function edit($id,$request,$response)
    {
+       $template = Templates::find($id);
 
+       if (!$template) return $this->view->render($response,'404.twig');
+
+       $category = Category::get();
+
+       return $this->view->render($response,'dashboard/template/create_edit.twig', compact('category','template'));
    }
 
    public function update()
--- app/routes.php
+++ app/routes.php
@@ -31,23 +31,21 @@ $app->group('',function () use ($container)  {
 
     $this->get('/','TemplateController:index')->setName('admin.main');
 
-
-
         $this->group('/template',function () use ($container)  {
             $this->get('/create','TemplateController:create')->setName('admin.template.create');
             $this->post('/store','TemplateController:store')->setName('admin.template.store');
-            $this->get('/edit','TemplateController:edit')->setName('admin.template.edit');
+            $this->get('/edit/{id:[0-9]+}','TemplateController:edit')->setName('admin.template.edit');
             $this->put('/update','TemplateController:update')->setName('admin.template.update');
-            $this->delete('/destroy','TemplateController:destroy')->setName('admin.template.destroy');
+            $this->delete('/destroy/{id:[0-9]+}','TemplateController:destroy')->setName('admin.template.destroy');
         });
 
         $this->group('/subscribers',function () use ($container)  {
             $this->get('/','SubscribersController:index')->setName('admin.subscribers.index');
             $this->get('/create','SubscribersController:create')->setName('admin.subscribers.create');
             $this->post('/store','SubscribersController:store')->setName('admin.subscribers.store');
-            $this->get('/edit','SubscribersController:edit')->setName('admin.subscribers.edit');
+            $this->get('/edit/{id:[0-9]+}','SubscribersController:edit')->setName('admin.subscribers.edit');
             $this->get('/update','SubscribersController:update')->setName('admin.subscribers.update');
-            $this->delete('/destroy','SubscribersController:destroy')->setName('admin.subscribers.destroy');
+            $this->delete('/destroy/{id:[0-9]+}','SubscribersController:destroy')->setName('admin.subscribers.destroy');
             $this->get('/import','SubscribersController:import')->setName('admin.subscribers.import');
             $this->get('/export','SubscribersController:export')->setName('admin.subscribers.export');
         });
@@ -56,18 +54,18 @@ $app->group('',function () use ($container)  {
             $this->get('/','CategoryController:index')->setName('admin.category.index');
             $this->get('/create','CategoryController:create')->setName('admin.category.create');
             $this->post('/store','CategoryController:store')->setName('admin.category.store');
-            $this->get('/edit','CategoryController:edit')->setName('admin.category.edit');
+            $this->get('/edit/{id:[0-9]+}','CategoryController:edit')->setName('admin.category.edit');
             $this->put('/update','CategoryController:update')->setName('admin.category.update');
-            $this->delete('/destroy','CategoryController:destroy')->setName('admin.category.destroy');
+            $this->delete('/destroy/{id:[0-9]+}','CategoryController:destroy')->setName('admin.category.destroy');
         });
 
         $this->group('/users',function () use ($container)  {
             $this->get('/','UsersController:index')->setName('admin.users.index');
             $this->get('/create','UsersController:create')->setName('admin.users.create');
             $this->post('/store','UsersController:store')->setName('admin.users.store');
-            $this->get('/edit','UsersController:edit')->setName('admin.users.edit');
+            $this->get('/edit/{id:[0-9]+}','UsersController:edit')->setName('admin.users.edit');
             $this->put('/update','UsersController:update')->setName('admin.users.update');
-            $this->delete('/destroy','UsersController:destroy')->setName('admin.users.destroy');
+            $this->delete('/destroy/{id:[0-9]+}','UsersController:destroy')->setName('admin.users.destroy');
         });
 
         $this->group('/log',function () use ($container)  {
@@ -80,13 +78,10 @@ $app->group('',function () use ($container)  {
             $this->get('/','SettingsController:index')->setName('admin.settings.index');
             $this->get('/create','SettingsController:create')->setName('admin.settings.create');
             $this->post('/store','SettingsController:store')->setName('admin.settings.store');
-            $this->get('/edit','SettingsController:edit')->setName('admin.settings.edit');
+            $this->get('/edit/{id:[0-9]+}','SettingsController:edit')->setName('admin.settings.edit');
             $this->put('/update','SettingsController:update')->setName('admin.settings.update');
-            $this->delete('/destroy','SettingsController:destroy')->setName('admin.settings.destroy');
+            $this->delete('/destroy/{id:[0-9]+}','SettingsController:destroy')->setName('admin.settings.destroy');
         });
-
-
-
 
 
 })->add(new AuthMiddleware($container));
